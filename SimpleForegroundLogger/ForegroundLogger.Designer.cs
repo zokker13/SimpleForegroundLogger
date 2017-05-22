1015b7406a717d55c783e8dbd9d7c524f89e3793
zokker13 <zokker13@posteo.de>
2017-05-22 19:58:14 +0200

    Improve gitignore and add project entirely

---
+++ SimpleForegroundLogger/ForegroundLogger.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace SimpleForegroundLogger
+{
+    partial class ForegroundLogger
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ForegroundLogger));
+            this.tmrMainTick = new System.Windows.Forms.Timer(this.components);
+            this.niMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.msMainStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.msiShow = new System.Windows.Forms.ToolStripMenuItem();
+            this.msiShowLogfile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.msiTruncateLog = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.msiKillMe = new System.Windows.Forms.ToolStripMenuItem();
+            this.pcbKillMe = new System.Windows.Forms.PictureBox();
+            this.msMainStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbKillMe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tmrMainTick
+            // 
+            this.tmrMainTick.Interval = 1000;
+            this.tmrMainTick.Tick += new System.EventHandler(this.tmrMainTick_Tick);
+            // 
+            // niMain
+            // 
+            this.niMain.ContextMenuStrip = this.msMainStrip;
+            this.niMain.Icon = ((System.Drawing.Icon)(resources.GetObject("niMain.Icon")));
+            this.niMain.Text = "Mr. Foreground Logger";
+            this.niMain.Visible = true;
+            // 
+            // msMainStrip
+            // 
+            this.msMainStrip.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.msMainStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msiShow,
+            this.msiShowLogfile,
+            this.toolStripSeparator1,
+            this.msiTruncateLog,
+            this.toolStripSeparator2,
+            this.msiKillMe});
+            this.msMainStrip.Name = "msMainStrip";
+            this.msMainStrip.ShowImageMargin = false;
+            this.msMainStrip.Size = new System.Drawing.Size(136, 104);
+            // 
+            // msiShow
+            // 
+            this.msiShow.Name = "msiShow";
+            this.msiShow.Size = new System.Drawing.Size(135, 22);
+            this.msiShow.Text = "Show";
+            this.msiShow.Click += new System.EventHandler(this.msiShow_Click);
+            // 
+            // msiShowLogfile
+            // 
+            this.msiShowLogfile.Name = "msiShowLogfile";
+            this.msiShowLogfile.Size = new System.Drawing.Size(135, 22);
+            this.msiShowLogfile.Text = "Show Logfile";
+            this.msiShowLogfile.Click += new System.EventHandler(this.msiShowLogfile_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(132, 6);
+            // 
+            // msiTruncateLog
+            // 
+            this.msiTruncateLog.Name = "msiTruncateLog";
+            this.msiTruncateLog.Size = new System.Drawing.Size(135, 22);
+            this.msiTruncateLog.Text = "Truncate Logfile";
+            this.msiTruncateLog.Click += new System.EventHandler(this.msiTruncateLog_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(132, 6);
+            // 
+            // msiKillMe
+            // 
+            this.msiKillMe.Name = "msiKillMe";
+            this.msiKillMe.Size = new System.Drawing.Size(135, 22);
+            this.msiKillMe.Text = "Kill Me, Senpaii";
+            this.msiKillMe.Click += new System.EventHandler(this.msiKillMe_Click);
+            // 
+            // pcbKillMe
+            // 
+            this.pcbKillMe.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pcbKillMe.Image = ((System.Drawing.Image)(resources.GetObject("pcbKillMe.Image")));
+            this.pcbKillMe.Location = new System.Drawing.Point(12, 12);
+            this.pcbKillMe.Name = "pcbKillMe";
+            this.pcbKillMe.Size = new System.Drawing.Size(230, 230);
+            this.pcbKillMe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcbKillMe.TabIndex = 1;
+            this.pcbKillMe.TabStop = false;
+            // 
+            // ForegroundLogger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 254);
+            this.Controls.Add(this.pcbKillMe);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ForegroundLogger";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mr. Foreground Logger";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.ForegroundLogger_Resize);
+            this.msMainStrip.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbKillMe)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrMainTick;
+        private System.Windows.Forms.NotifyIcon niMain;
+        private System.Windows.Forms.ContextMenuStrip msMainStrip;
+        private System.Windows.Forms.ToolStripMenuItem msiKillMe;
+        private System.Windows.Forms.ToolStripMenuItem msiTruncateLog;
+        private System.Windows.Forms.ToolStripMenuItem msiShowLogfile;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem msiShow;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.PictureBox pcbKillMe;
+    }
+}
+
---
+++ SimpleForegroundLogger/ForegroundLogger.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimpleForegroundLogger
+{
+    struct Report
+    {
+        public string Message;
+        public DateTime Time;
+
+        public Report(string message)
+        {
+            Message = message;
+            Time = DateTime.Now;
+        }
+    }
+
+    public partial class ForegroundLogger : Form
+    {
+        private readonly Queue<Report> _messages = new Queue<Report>();
+        private readonly ForegroundWatcher _foregroundWatcher = new ForegroundWatcher();
+        private const string FILE_LOG = "foreground_log.txt";
+        
+        public ForegroundLogger()
+        {
+            InitializeComponent();
+        }
+
+        private void _foregroundWatcher_ForegroundChanged(object sender, ForegroundArgs e)
+        {
+            string text = $"{e.Process.ProcessName.Truncate(16),20}";
+            _messages.Enqueue(new Report($"{text}   \"{e.Process.MainWindowTitle}\""));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tmrMainTick.Enabled = true;
+
+            _foregroundWatcher.Hook();
+            _foregroundWatcher.ForegroundChanged += _foregroundWatcher_ForegroundChanged;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            FlushQueue(_messages);
+        }
+
+
+        private async void WriteTextAsync(string text)
+        {
+            byte[] encodedText = Encoding.UTF8.GetBytes(text);
+
+            using (FileStream fs = new FileStream(
+                FILE_LOG
+                , FileMode.Append
+                , FileAccess.Write
+                , FileShare.None
+                , 4096
+                , true
+            ))
+            {
+               await fs.WriteAsync(encodedText, 0, encodedText.Length);
+            }
+        }
+
+        private void FlushQueue(Queue<Report> queue)
+        {
+            string text = "";
+
+            while (queue.Count > 0)
+            {
+                Report message = queue.Dequeue();
+                text = $"{text}[{message.Time.ToString("s")}]   {message.Message}\n";
+            }
+            
+            WriteTextAsync(text);
+        }
+
+        private void tmrMainTick_Tick(object sender, EventArgs e)
+        {
+            FlushQueue(_messages);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _foregroundWatcher.Unhook();
+            niMain.Visible = false;
+            tmrMainTick.Enabled = false;   
+        }
+
+        private void msiKillMe_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void msiTruncateLog_Click(object sender, EventArgs e)
+        {
+            File.Delete(FILE_LOG);
+        }
+
+        private void msiShowLogfile_Click(object sender, EventArgs e)
+        {
+            Process.Start(FILE_LOG);
+        }
+
+        private void msiShow_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void ForegroundLogger_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Adds nice dots after the function exceeds the defined amount of letters.
+        /// Shamelessly stolen from: https://stackoverflow.com/a/6724896/2670428
+        /// </summary>
+        /// <param name="value">Our string to be truncated.</param>
+        /// <param name="maxChars">The amount of letters we want to have (max.)</param>
+        /// <returns>Returns the truncated string with proper dots at the end.</returns>
+        public static string Truncate(this string value, int maxChars)
+        {
+            return value.Length <= maxChars ? value : value.Substring(0, maxChars) + " ...";
+        }
+    }
+
+}
